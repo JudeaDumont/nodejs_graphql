802a245f42265ad7b9c6e304a2141e0408f70633
dumont, judea <dumontjudea2@gmail.com>
2024-06-19 12:21:50 -0700

    I'm going with a nodejs implementation of graphql just for the sake of learning graphql alongside javascript. the spring boot tutorial for graphql was terrible.

---
+++ FakeData.js
@@ -0,0 +1,18 @@
+const UserList = [
+    {
+        id: 1,
+        name: "Jeff",
+        username:"BallHog",
+        age:"15",
+        nationality:"Jew"
+    },
+    {
+        id: 2,
+        name: "Jack",
+        username:"BigBall",
+        age:"27",
+        nationality:"Chinese"
+    },
+]
+
+module.exports = {UserList}
