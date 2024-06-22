52f05a60a4b9b5fa54df49475b10cf4c0ddb121b
dumont, judea <dumontjudea2@gmail.com>
2024-06-21 21:15:23 -0700

    functioning react mutation for creating a user

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {gql, useLazyQuery, useQuery} from "@apollo/client";
+import {gql, useLazyQuery, useMutation, useQuery} from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
     query AllUsersQuery {
@@ -17,6 +17,7 @@ const QUERY_ALL_USERS = gql`
     }
 `
 
+//this is case-sensitive
 const QUERY_ALL_MOVIES = gql`
     query AllMoviesQuery{
         movies{
@@ -35,7 +36,15 @@ const QUERY_MOVIE_BY_NAME = gql`
         }
     }
 `
-
+const CREATE_USER_MUTATION = gql`
+    mutation CreateUser($createUserInput:CreateUserInput!){
+        createUser(input: $createUserInput){
+            id
+            name
+            age
+        }
+    }
+`
 function mapToH1UserText(allUsers) {
     return allUsers && allUsers.users.map((user) => {
         return (
@@ -59,16 +68,40 @@ function mapToH1MovieText(allMovies) {
 }
 
 function DisplayData() {
+
+    const [createUser] = useMutation(CREATE_USER_MUTATION)
+
     const allUsers = useQuery(QUERY_ALL_USERS).data
     const allMovies = useQuery(QUERY_ALL_MOVIES).data
     const [fetchMovie, {data: movieSearchedData, error: movieError}] =
         useLazyQuery(QUERY_MOVIE_BY_NAME)
 
     const [movieSearched, setMovieSearched] = useState("")
+
+
+    const [name, setName] = useState("")
+    const [username, setUsername] = useState("")
+    const [age, setAge] = useState(0)
+    const [nationality, setNationality] = useState("")
+
     if (allUsers) {
         console.log(allUsers)
     }
     return <div>
+        <div>
+            <input type="text" placeholder="Name..." onChange={
+                (event)=>{setName(event.target.value)}}></input>
+            <input type="text" placeholder="Username..." onChange={
+                (event)=>{setUsername(event.target.value)}}></input>
+            <input type="number" placeholder="Age..." onChange={
+                (event)=>{setAge(Number(event.target.value))}}></input>
+            <input type="text" placeholder="Nationality..." onChange={
+                (event)=>{setNationality(event.target.value)}}></input>
+            <button onClick={()=>{
+                createUser({variables:{createUserInput:{name, username, age, nationality}}})
+            }}>Create User</button>
+        </div>
+
         {mapToH1UserText(allUsers)}
         {mapToH1MovieText(allMovies)}
         {
